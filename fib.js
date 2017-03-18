8b8a82292bfcef85d367e7b14b75b097e87cfdad
Kareena Hudson <kareena.k.hudson@aggiemail.usu.edu>
2017-03-18 03:48:05 +0000

    Updated pell files

--- fib.js
+++ fib.js
@@ -190,52 +190,15 @@ var pell = function (n, node) {
 	  node.setAttribute("id", "pell");
 }
 
-function fibHelper(n) {
-	var value;
-	var div = document.createElement('div');
-	div.setAttribute("class", "fib");
-
-	// leaf nodes aka. base case
-	if (n < 2) {
-		if (n === 0) {
-			value = 0;
-		} 
-		else if (n === 1) {
-			value = 1;
-		}
-		var p = document.createElement('p');
-		p.textContent = 'Fib(' + n + ') = ' + value;
-		div.appendChild(p)
-	} 
-	else {
-		var left = fibHelper(n - 1);
-		var clas = left.html.getAttribute("class");
-		left.html.setAttribute("class", clas + " fib-left");
-		
-		var right = fibHelper(n - 2);
-		clas = right.html.getAttribute("class");
-		right.html.setAttribute("class", clas + " fib-right");
-
-		value = left.value + right.value;
-		var p = document.createElement('p');
-		p.textContent = 'Fib(' + n + ') = ' + value;
-		div.appendChild(p);
-
-		div.appendChild(left.html);
-		div.appendChild(right.html);
-	}
-	return { 'value': value, 'html': div };
-}
-var fib = function (n, node) {
-	var tree = fibHelper(n)
-		node.appendChild(tree.html);
-		//node.style = "display: inline-block;";
-	  node.setAttribute("id", "fib");
-}
-
 document.querySelector('body').appendChild(style);
+
+( function(color, id) {
+	var div = document.createElement('div');
+	div.setAttribute('class', color + ' shadowed stuff-box');
+	div.setAttribute('id', id);
+	document.body.appendChild(div);
+}('red', 'pell'));
 
 pell(9, document.querySelector('.red'))
 pell(10, document.querySelector('.blue'))
 pell(11, document.querySelector('.yellow'))
-
